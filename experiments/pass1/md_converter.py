2a684150d2e1b57434491d644263349f69592d65
Michael Granado <michaelgranado8@gmail.com>
2025-03-08 16:18:36 -0800

    New Style

--- experiments/pass1/md_converter.py
+++
@@ -1,8 +0,0 @@
-import markdown
-
-with open("logic.txt", "r") as input_file:
-    text = input_file.read()
-html = markdown.markdown(text)
-
-with open("logic.html", "w", errors="xmlcharrefreplace") as output_file:
-    output_file.write(html)
